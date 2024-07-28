224907616548d172ec1a04025ea388a2e0b62334
Soumya <kesharwanisoumya10@gmail.com>
2024-07-28 16:37:48 +0530

    initial commit

---
+++ src/app/workout-input/workout-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-input',
+  templateUrl: './workout-input.component.html',
+  styleUrl: './workout-input.component.css'
+})
+export class WorkoutInputComponent {
+
+}
